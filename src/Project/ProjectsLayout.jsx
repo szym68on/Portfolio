7562da44681bbab85a77297b6c2f245a9924ef37
Szymon Świercz <swierczszymon2001@wp.pl>
2024-02-10 16:04:00 +0100

    change fake api to const date

--- src/Project/ProjectsLayout.jsx
+++ src/Project/ProjectsLayout.jsx
@@ -1,21 +1,39 @@
-import { useEffect, useState } from "react";
 import ItemProject from "./ItemProject";
 
+const projects = [
+  {
+    description: "SystemLMS- Join to us",
+    code_url: "https://github.com/amrusb/lms-pz",
+    img_path: "/images/systemLms-app.png",
+    demo_url: "#",
+  },
+  {
+    description: "BudgetApp - Manage your  budget",
+    code_url: "https://github.com/szym68on/Budget-App",
+    img_path: "/images/budget-app.png",
+    demo_url: "https://budgetdemoapp.netlify.app/",
+  },
+  {
+    description: "QuizzApp - Take a quiz on various fields",
+    code_url: "https://github.com/szym68on/Quiz-App",
+    img_path: "/images/quiz-app.png",
+    demo_url: "https://quizzzappdemo.netlify.app/",
+  },
+  {
+    description: "Portfolio - Get to know me",
+    code_url: "https://github.com/szym68on/Portfolio",
+    img_path: "/images/portfolio.png",
+    demo_url: "#",
+  },
+  {
+    description: "MovieApp - Search your favorite movie",
+    code_url: "https://github.com/szym68on/Movie-app",
+    img_path: "/images/movie-app.png",
+    demo_url: "https://movieappdemoo.netlify.app/",
+  },
+];
+
 function ProjectsLayout() {
-  const [projects, setProjects] = useState(null);
-  useEffect(function () {
-    const getData = async function () {
-      try {
-        const data = await fetch("http://localhost:3001/projects");
-        if (!data.ok) throw new Error("Something went wrong with projects!");
-        const res = await data.json();
-        setProjects(res);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    getData();
-  }, []);
   return (
     <section className="w-[100%] mx-auto mt-5 grid grid-cols-1 md:grid-cols-2 place-items-center gap-y-5 animate-fade-in">
       {projects
